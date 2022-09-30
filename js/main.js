c1fc49c1412f579989571a382f30715aa26dc29d
Денис Антропов <denisandreevich93@gmail.com>
2022-09-30 23:13:28 +0300

    desktop ver 1.01

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function renderPopup() {
   hambPopup.appendChild(menu);
 };
 
-// Код для закрытия меню при нажатии на ссылку
+// Закрытие меню при нажатии на ссылку
 
 const links = Array.from(menu.children);
 
@@ -35,32 +35,49 @@ function closeOnClick() {
 
 // popup оставить заявку
 
-const openPopup = document.getElementById('popup__open'); // ссылка открытия
-const closePopup = document.getElementById('popup__close'); //ссылка закрытия
-const popup = document.getElementById('popup'); // тело popup
-const popupBody = document.getElementById('popup');
-const bodyPopup = document.body;
+var openModals = document.querySelector('.hero__btn'),
+  modal = document.querySelector('.popup'),
+  overlay = document.querySelector('.overlay'),
+  close = document.querySelector('.popup__close');
+bodyPopup = document.body;
 
-openPopup.addEventListener('click', function (e) {
-  e.preventDefault();
-  popup.classList.add('active');
-  bodyPopup.classList.toggle("noscroll");
+openModals.addEventListener("click", popupOpen);
+close.addEventListener("click", popupClose);
+overlay.addEventListener("click", popupClose);
 
-});
+function popupOpen(q) {
+  modal.classList.add('open');
+  overlay.classList.add('open');
+  bodyPopup.classList.add('noscroll');
+}
 
-closePopup.addEventListener('click', () => {
-  popup.classList.remove('active');
-  bodyPopup.classList.remove("noscroll");
-});
+function popupClose(q) {
+  modal.classList.remove('open');
+  overlay.classList.remove('open');
+  bodyPopup.classList.remove('noscroll');
+}
 
-popupBody.addEventListener('click', () => {
-  popup.classList.remove('active');
-  bodyPopup.classList.remove("noscroll");
-});
+// openModal.onclick = function () {
+//   modal.classList.add('open');
+//   overlay.classList.add('open');
+//   bodyPopup.classList.add('noscroll');
+// }
+
+// close.onclick = function () {
+//   modal.classList.remove('open');
+//   overlay.classList.remove('open');
+//   bodyPopup.classList.remove('noscroll');
+// }
+
+// overlay.onclick = function () {
+//   modal.classList.remove('open');
+//   this.classList.remove('open');
+//   bodyPopup.classList.remove('noscroll');
+// }
 
 //Плавная прокрутка до нужной секции
 
-$('.header__nav a, .footer__nav a').on('click', function () {
+$('.header__nav a, .footer__nav a, .hero__slider-link').on('click', function () {
   let href = $(this).attr('href');
 
   $('html, body').animate({
@@ -96,33 +113,39 @@ $(function () {
 $(function () {
   $(window).scroll(function () {
     if ($(window).scrollTop() > 400) {
-      $('.scroll_top').show();
+      $('.scroll-top').show();
     } else {
-      $('.scroll_top').hide();
+      $('.scroll-top').hide();
     }
   });
 
-  $('.scroll_top').click(function () {
+  $('.scroll-top').click(function () {
     $('html, body').animate({ scrollTop: 0 }, 600);
     return false
   });
 });
 
+// Аккордион
+
 $(function () {
   $("#accordion").accordion({
     active: false,
     heightStyle: "content",
-    collapsible: true
+    collapsible: true,
   });
 })
+
+// Табы
 
 $("#tabs").tabs({
   show: { effect: "blind", duration: 800 },
   hide: { effect: "blind", duration: 800 },
   heightStyle: "content",
   collapsible: true,
-  active: false
+  active: false,
 });
+
+// Слайдер
 
 $(function () {
   $('.gallery__list').slick({
@@ -142,3 +165,49 @@ $(function () {
     ]
   });
 });
+
+// Открыть модальном
+function openModal() {
+  document.getElementById("myModal").classList.add('visible');
+  document.querySelector(".gallery__overlay").classList.add('visible');
+  document.body.classList.add('noscroll');
+}
+
+// Закрыть модальное
+function closeModal() {
+  document.getElementById("myModal").classList.remove('visible');
+  document.querySelector(".gallery__overlay").classList.remove('visible');
+  document.body.classList.remove('noscroll');
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Вперед/назад контроль
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  return false
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  return false
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("gallery__slides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("gallery__actives", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += "gallery__actives";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
